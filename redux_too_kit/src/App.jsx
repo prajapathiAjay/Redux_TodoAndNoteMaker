3fd4a2be766dc8aa96c63330b9c86c5cf7e27be2
prajapathiAjay <ajayworking6309@gmail.com>
2025-11-01 21:56:10 +0530

    updates

--- redux_too_kit/src/App.jsx
+++ redux_too_kit/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Fragment } from "react";
+import { Provider } from "react-redux";
+import TodoForm from "./components/ToDoForm/ToDoForm.jsx";
+import TodoList from "./components/ToDoList/ToDoList.jsx";
+import {store} from "./redux/store";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import './App.css';
+import NoteForm from "./components/NoteForm/NoteForm";
+import NoteList from "./components/NoteList/NoteList";
+import Home from "./components/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Provider store={store}>
+        <BrowserRouter>
+        <Routes>
+          <Route path="" element={<Home />}>
+            
+          </Route>
+          <Route path="todo"
+          element={
+            <Fragment>
+              <NavBar />
+             <h1>To Dos</h1>
+              <TodoForm  />
+              <TodoList />
+            </Fragment>
+          }>
+
+          </Route>
+          <Route path="notes"
+          element={
+            <Fragment>
+               <NavBar />
+               <h1>Notes</h1>
+              <NoteForm  />
+              <NoteList />
+            </Fragment>
+          }>
+            
+            </Route>
+        </Routes>
+       
+        </BrowserRouter>
+      </Provider>
+    </div>
+  );
 }
 
-export default App
+export default App;
